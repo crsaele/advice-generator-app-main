47a983210617d6324065de2439f9f27b6740f721
Carlos Al <crsaele@gmail.com>
2024-11-06 16:54:13 -0600

    HTML CSS JS code refactor

--- advice.js
+++ advice.js
@@ -1,29 +1,29 @@
 // Store the button to add the event and the div element that will show the advice
-let adviceElement = document.getElementById('advice-element');
-let adviceButton = document.getElementById('advice-btn');
+let advice = document.getElementById('advice');
+let diceButton = document.getElementById('dice-btn');
 
 
 // Get advice function fetch's the advice and shows it via inner html
 const getAdvice = () => {
-    adviceElement.innerHTML = "<p>Loading...</p>";
+    advice.innerHTML = "<p>Loading...</p>";
   
     fetch("https://api.adviceslip.com/advice")
       .then(response => response.json())
       .then(data => {
-        adviceElement.innerHTML = `
+        advice.innerHTML = `
           <h2>A D V I C E #${data.slip.id}</h2>
           <blockquote>"${data.slip.advice}"</blockquote>
         `;
       })
       .catch(err => {
-        adviceElement.innerHTML = `
+        advice.innerHTML = `
             <p>${err.message}.</p>
         `;
       });
   };
   
   // Adds an event to the dice button
-  adviceButton.addEventListener("click", getAdvice);
+  diceButton.addEventListener("click", getAdvice);
   
   // Generates an advice when refresh
   getAdvice();
